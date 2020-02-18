b5d2b8cedc83fac08e7adc2d643d030919554f1a
Julian -Home <j.henry.b.orr@gmail.com>
2020-02-17 22:04:25 -0500

    welcome! added view for frontdoor

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\julian\projects\django_projects\dj_curio\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
